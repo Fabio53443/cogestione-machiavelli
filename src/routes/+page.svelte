6e15dc00f0b345887a25c4ff635f3bc70d1938ee
Fabio Massimo Proietti <fabiomassimoproietti1@gmail.com>
2025-02-08 15:59:53 +0100

    spallanzani

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,10 @@
 
 <div class="max-w-2xl mx-auto text-center">
     <h1 class="text-4xl font-bold mb-6 text-[#FB773C]">
-        Benvenuto all'autogestione del Liceo Machiavelli
+        Benvenuto alla cogestione del Liceo Spallanzani
     </h1>
     <p class="text-xl mb-8 text-gray-300">
-        La piattaforma per la gestione dei corsi e delle prenotazioni. Accedi qui:<br /> 
+        La piattaforma per la gestione dei corsi e delle prenotazioni. Segui <a href="https://instagram.com/rappresentanti_liceospalla"> @rappresentanti_liceospalla</a>per restare aggiornato! Accedi qui:<br /> 
     </p>
 
 

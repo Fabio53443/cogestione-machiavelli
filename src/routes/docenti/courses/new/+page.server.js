c5e97617c22412038a1ed3e6b0e1e049cd117fd2
Fabio Massimo Proietti <fabiomassimoproietti1@gmail.com>
2025-02-11 16:15:16 +0100

    fix new course when admin

--- src/routes/docenti/courses/new/+page.server.js
+++ src/routes/docenti/courses/new/+page.server.js
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
+import { isAdmin } from '$lib/isAdmin';
 export function load({ locals }) {
-  if (!locals.user || locals.user.role !== 'docente') {
+
+  if (!locals.user || locals.user.role !== 'docente' && !isAdmin(locals.user)) {
     throw redirect(302, '/');
   }
     return {
